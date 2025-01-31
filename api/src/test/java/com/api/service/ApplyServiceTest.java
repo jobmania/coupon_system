d533f99adb303424ac29b9cce0b0e72d33d2fa64
jobmania <xzxz7003@naver.com>
2025-02-01 00:58:05 +0900

    모듈 만들기

---
+++ api/src/test/java/com/api/service/ApplyServiceTest.java
@@ -0,0 +1,64 @@
+package com.api.service;
+
+import com.api.repository.CouponRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+
+    @Test
+    public void 한번만응모() {
+        // 요청이 여러개 들어오면 당연하게
+        //
+        applyService.apply(1L);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러명요청() throws InterruptedException {
+        // 요청(1000개)이 여러개 들어오면 당연하게 멀티쓰레드
+        // 100 개의 쿠폰.
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+
+        // 더 많은 쿠폰이 발급
+        // 트랜잭션 (레이스 컨디션)
+        assertThat(count).isEqualTo(100);
+    }
+
+}
